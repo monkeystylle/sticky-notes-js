c10f4703d5b5860437a9c4ba000495b39723edfc
meng <monkeystylle@gmail.com>
2022-10-30 07:23:00 +0800

    feat: add note counter

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 const btnAddNote = document.querySelector('.add-note');
 const notesContainer = document.querySelector('.notes-container');
+const notesCountLabel = document.querySelector('.note-count');
 
 function createNoteElement() {
   const note = document.createElement('div');
@@ -7,7 +8,7 @@ function createNoteElement() {
   const body = document.createElement('textarea');
 
   //add class to the node
-  note.classList.add('note', 'note-style-1');
+  note.classList.add('note', getRandomNoteStyle());
 
   //adding attribute to the input field
   title.classList.add('note-title');
@@ -27,8 +28,23 @@ function createNoteElement() {
   return note;
 }
 
+//generate random color of note sheet
+function getRandomNoteStyle() {
+  const styles = [1, 2, 3];
+  const randomStyle = styles[Math.floor(Math.random() * styles.length)];
+
+  return `note-style-${randomStyle}`;
+}
+
+function updateNotesCount() {
+  const notesCount = notesContainer.children.length;
+  notesCountLabel.textContent = `${notesCount} note`;
+}
+
+//add note
 btnAddNote.addEventListener('click', () => {
   const note = createNoteElement();
 
   notesContainer.appendChild(note);
+  updateNotesCount();
 });
